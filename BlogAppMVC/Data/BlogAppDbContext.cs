a98e745849caeed629119142a39bb24b3ea3c857
Eliab <eliablopez@hotmail.com>
2024-06-06 07:34:29 -0500

    Agregar archivos de proyecto.

---
+++ BlogAppMVC/Data/BlogAppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BlogAppMVC.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogAppMVC.Data
+{
+    public class BlogAppDbContext : DbContext
+    {
+        public BlogAppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+    }
+}
