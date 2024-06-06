071089f0465ace872affee8edb4595079b3551a4
Eliab <eliablopez@hotmail.com>
2024-06-06 15:37:32 -0500

    Add form

---
+++ BlogAppMVC/Controllers/AdminTagsController.cs
@@ -0,0 +1,37 @@
+﻿using BlogAppMVC.Data;
+using BlogAppMVC.Models.Domain;
+using BlogAppMVC.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogAppMVC.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        private readonly BlogAppDbContext _blogAppDbContext;
+        public AdminTagsController(BlogAppDbContext blogAppDbContext)
+        {
+            _blogAppDbContext = blogAppDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+            // Mapping AddTagRequest to Tag domain model
+            var name = addTagRequest.Name;
+            var displayName = addTagRequest.DisplayName;
+
+            var tag = new Tag { Name = name, DisplayName = displayName };
+
+            _blogAppDbContext.Tags.Add(tag);
+            _blogAppDbContext.SaveChanges();
+            
+            return View("Add");
+        }
+    }
+}
---
+++ BlogAppMVC/Models/ViewModels/AddTagRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace BlogAppMVC.Models.ViewModels
+{
+    public class AddTagRequest
+    {
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+    }
+}
